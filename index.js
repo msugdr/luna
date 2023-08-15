efa57373dbdb66fbd4b0f14b2cc4c96005d3a03a
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-15 21:41:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 const fs = require('fs');
 var dat;
 const server = http.createServer((request, response) => { 
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  dat=fs.readFileSync('text.txt','utf8');
+  response.writeHead(200, {"Content-Type": "img/png"});
+  dat=fs.readFileSync('girl.png','binary');
   response.end(dat);
 });
 server.listen(process.env.PORT);
