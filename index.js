9ef0e052dfd4f9edf65d990e3f3e8a7b2a093da7
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-15 20:20:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const http = require('http');
-var msg;
 const server = http.createServer((request, response) => { 
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  msg = " URI : " + request.url; 
-  response.end(msg);});
-const port = process.env.PORT || 1337;
-server.listen(port);
-console.log("Server running at http://localhost:%d", port);
+  response.end("Hello World");
+});
+server.listen(process.env.PORT);
+
