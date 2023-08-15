b9ee9e77a2a219fdba1a2ddb1d88af6310890053
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-15 21:31:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 var dat;
 const server = http.createServer((request, response) => { 
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   dat=fs.readFileSync('text.txt','utf8');
   response.end(dat);
 });
