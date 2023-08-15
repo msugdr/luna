d511b6d6ee3c714c73a131afdc5c952e8e2593fa
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-15 21:09:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const server = http.createServer((request, response) => { 
+  response.writeHead(400, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
 server.listen(process.env.PORT);
