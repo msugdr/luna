d953d47e98c39ced97e08bd377e03728c1d1f1a7
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-16 11:52:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ const server = http.createServer((request, response) => {
   msg = decodeURIComponent(msg);
   sendRequest(msg);
   response.write (" URL : " + request.url + "\n");
-  response.end(   " MSG : " + msg + "\n"); 
+  dat = "<!DOCTYPE html><html><head><meta charset="utf8"></head><body>送信メッセージ：" . msg . "</body></html>";
+  response.end(dat);
+ // response.end(   " MSG : " + msg + "\n"); 
 });
 server.listen(process.env.PORT);
 
