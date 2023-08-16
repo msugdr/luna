17afb82184b01398bee5ef20eb4449db60ed4872
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-16 10:07:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const http = require('http');
-const requestx = require('request');
-const TOKEN = "0maOvAs8dtXu8h7eEPMXGk2VqMcj6LEztSP9C7kudOg"
-const message = `これはテストですよ`;
-sendRequest(`デプロイされました`); // デプロイされたときに実行される
-function sendRequest (message) {
-  const options = {
-    uri: 'https://notify-api.line.me/api/notify',
-    headers: {
-      'Authorization': `Bearer ${TOKEN}`
-    },
-    form: {
-      ‘message’ : message
-    }
-  };
-  requestx.post(options, (error, response, body) => {
-    if (error) {
-      return;
-    }
-  });
-}
+var msg;
+const server = http.createServer((request, response) => { 
+  response.write(" URI    : " + request.url + "\n"); 
+  response.write(" METHOD : " + request.method + "\n"); 
+      if (request.method === 'POST'){
+        var data = '';
+        request.on('data', function(chunk) {data += chunk})
+                   .on('end', function() {
+                              response.end(' POST DATA : ' + data);
+                    })
+      } else {
+        response.end();
+      }
+});
+server.listen(process.env.PORT);
+
 
 
 
