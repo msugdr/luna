f8a29b5224883918b4884d9039ea2b3721218cab
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-16 12:04:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var msg;
 const server = http.createServer((request, response) => { 
   [dum, msg] = request.url.split("=");
   msg = decodeURIComponent(msg);
+  msg = msg.replace('a', 'b');
   sendRequest(msg);
   response.write (" URL : " + request.url + "\n");
   response.end(   " MSG : " + msg + "\n"); 
