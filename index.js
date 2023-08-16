6dfa8439214c7a50eabad7e08ae1957e640744bd
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-16 10:10:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const http = require('http');
 var msg;
 const server = http.createServer((request, response) => { 
-  response.write(" URI    : " + request.url + "\n"); 
-  response.write(" METHOD : " + request.method + "\n"); 
-      if (request.method === 'POST'){
-        var data = '';
-        request.on('data', function(chunk) {data += chunk})
-                   .on('end', function() {
-                              response.end(' POST DATA : ' + data);
-                    })
-      } else {
-        response.end();
-      }
+  response.end(" URI    : " + request.url + "\n"); 
 });
 server.listen(process.env.PORT);
 
